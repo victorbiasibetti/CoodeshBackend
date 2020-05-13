c73c7676bd8b0d0de931e962ccd53d31edbbf874
Victor Biasibetti <victorbiasibetti7@gmail.com>
2020-05-12 22:41:22 -0300

    add eslint

--- src/server.js
+++ src/server.js
@@ -1,10 +1,8 @@
-const express = require('express')
+const express = require('express');
 
-const server = express()
+const server = express();
 
 
-server.get('/', (request, response) => {
-  return response.json({ok: true})
-})
+server.get('/', (request, response) => response.json({ ok: true }));
 
-server.listen(3000, () => console.log('server on'))
+server.listen(3000, () => console.log('server on'));
